0763d7c2f4053d16d1a98b0410c71e76447d2c52
EugeneC <eugene.cooney@gmail.com>
2016-01-21 14:23:04 +0000

    Final Upload

---
+++ Databases/StartDerby.bat
@@ -0,0 +1,5 @@
+c:
+
+cd Derby\lib
+
+java -jar -Dderby.system.home=C:\IntroJavaDev\Databases derbyrun.jar server start
---
+++ Databases/StartDerby_EC_2.bat
@@ -0,0 +1,6 @@
+c:
+
+cd JavaEEWorkspace\DatabasesDerby\lib
+
+java -jar -Dderby.system.home=C:\JavaEEWorkspace\Databases derbyrun.jar server start
+echo:
